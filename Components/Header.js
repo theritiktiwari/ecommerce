5b8f17cb5fcb0f40518b8b5717f01ec40d933596
Ritik Tiwari <ritiktiwariwork@gmail.com>
2022-06-05 01:06:14 +0530

    UI Changes

--- Components/Header.js
+++ Components/Header.js
@@ -11,7 +11,7 @@ const Header = (props) => {
     return (
         <>
             <header className="text-gray-600 body-font bg-white sticky top-0 z-10 shadow-md">
-                <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                <div className="container mx-auto flex flex-wrap px-5 pb-5 pt-5 flex-col md:flex-row items-center">
                     <Link href={"/"}>
                         <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-black rounded-full" viewBox="0 0 24 24">
@@ -20,30 +20,31 @@ const Header = (props) => {
                             <span className="ml-3 text-xl">{props.name}</span>
                         </a>
                     </Link>
-                    <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center space-x-4">
+                    <nav className="md:mx-auto px-5 flex flex-wrap items-center text-base justify-center">
                         <Link href={"/"}><a className="ml-2 mr-2 hover:text-gray-900">Home</a></Link>
                         <Link href={"/about"}><a className="ml-2 mr-2 hover:text-gray-900">About</a></Link>
                         <Link href={"/products"}><a className="ml-2 mr-2 hover:text-gray-900">All Products</a></Link>
                         <Link href={"/contact"}><a className="ml-2 mr-2 hover:text-gray-900">Contact</a></Link>
                         {!props.user.token && <Link href={"/auth"}><a className="ml-2 mr-2 hover:text-gray-900">Login</a></Link>}
                     </nav>
-                    <Link href={"/cart"}>
-                        <a className='absolute right-12 top-7 mr-3'><FaShoppingCart className='text-2xl' /></a>
-                    </Link>
-                    {props.user.token &&
-                        <>
-                            <a className='absolute right-20 top-7 mr-3'>
+                    <div className="absolute right-0 top-5 mx-5 px-5">
+                        <Link href={"/cart"}>
+                            <a className='absolute right-12 top-2 mr-3'><FaShoppingCart className='text-2xl' /></a>
+                        </Link>
+                        {props.user.token &&
+                            <a className='absolute right-20 top-2 mr-3 cursor-pointer'>
                                 <FaUserCircle onMouseOver={() => setDropDown(true)} onMouseLeave={() => setDropDown(false)} className='text-2xl' />
                             </a>
-
-                            {dropDown && <div onMouseOver={() => setDropDown(true)} onMouseLeave={() => setDropDown(false)} className='absolute right-20 top-14 md:top-12 px-5 py-2 w-36 bg-white  border shadow-lg'>
-                                <ul className=''>
-                                    <Link href={"/profile"}><a><li className='my-2'>My Account</li></a></Link>
-                                    <Link href={"/orders"}><a><li className='my-2'>My Orders</li></a></Link>
-                                    <li onClick={props.logout} className='cursor-pointer text-red-500 my-2'><a>Logout</a></li>
-                                </ul>
-                            </div>}
-                        </>
+                        }
+                    </div>
+                    {props.user.token && dropDown &&
+                        <div onMouseOver={() => setDropDown(true)} onMouseLeave={() => setDropDown(false)} className='absolute right-20 top-14 md:top-12 px-5 py-2 w-36 bg-white  border shadow-lg'>
+                            <ul>
+                                <Link href={"/profile"}><a><li className='my-2'>My Account</li></a></Link>
+                                <Link href={"/orders"}><a><li className='my-2'>My Orders</li></a></Link>
+                                <li onClick={props.logout} className='cursor-pointer text-red-500 my-2'><a>Logout</a></li>
+                            </ul>
+                        </div>
                     }
                 </div>
             </header>
